2c4fe7847e996ede80bb00745e7a641509be3d2d
karthikayan4u <54244642+karthikayan4u@users.noreply.github.com>
2021-03-15 09:22:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ from tensorflow.keras.preprocessing import image
 import numpy as np
 import PIL
 
-for i in os.listdir('./static/uploads'):
-	os.remove('./static/uploads/' + i)
+UPLOAD_FOLDER = 'static/uploads/'
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 IMAGE_SHAPE = (224, 224)
